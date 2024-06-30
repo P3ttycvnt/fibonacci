62da63f0bdac247d8848ea27bfe232e30fa067d0
p3ttycvnt <64262300+P3ttycvnt@users.noreply.github.com>
2024-06-30 03:01:21 -0400

    Update fibonacci.c

--- fibonacci/fibonacci.c
+++ fibonacci/fibonacci.c
@@ -2,20 +2,21 @@
 #include <stdlib.h>
 
 int main(){
+    // ints fucked up the program just printed negative numbers
     long morer = 1;
     long lesser = 0;
     long middle = 0;
     printf("0\n");
     for(int i = 0; i < 46; i++){
-    
+    //quick algorithm to print fibonacci
     printf("%d\n", morer);
     middle = lesser;
     lesser = morer;
     morer += middle;
 
-
-
-
+        
+    //some fucking space lol
+    //nothing special here
 
     }
 
